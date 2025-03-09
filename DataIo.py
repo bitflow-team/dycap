3175081c9afb28bab51dc6da091304840ca4f1e8
xdsun777 <charmgo@qq.com>
2025-03-10 03:31:11 +0800

    运行时依赖

--- DataIo.py
+++ DataIo.py
@@ -3,9 +3,29 @@ from config import RUNNING_CONFIG_KEYS,RUNNING_CONFIG_URLS,RUNNING_CONFIG_DIR
 
 class Txt:
     def __init__(self):
-        self._urls = RUNNING_CONFIG_URLS
-        self._keys = RUNNING_CONFIG_KEYS
-    
+        self._urls_file = RUNNING_CONFIG_URLS.as_posix()
+        self._keys_file = RUNNING_CONFIG_KEYS.as_posix()
+        self._urls =None
+        self._keys =None
+
+    def get_urls(self):
+        try:
+            with open(self._urls_file,'r',encoding='utf-8') as f:
+                self._urls = f.readlines()
+            self._urls = [u.strip('\n') for u in self._urls]
+            return self._urls
+        except FileNotFoundError as e:
+            raise e
+
+    def get_keys(self):
+        try:
+            with open(self._keys_file,'r',encoding='utf-8') as f:
+                self._keys = f.readlines()
+            self._keys = [u.strip('\n') for u in self._keys]
+            return self._keys
+        except FileNotFoundError as e:
+            raise e
+
 class Xlsx:
     def __init__(self):
         pass
@@ -20,4 +40,6 @@ class DyDB(Db):
         super().__init__()
 
 
-    
+if __name__ == '__main__':
+    a = Txt()
+    print(a.get_urls())
